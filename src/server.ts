ec54b96a730502ff1dc39ab17834eca3e4d60993
natancs <natan.carvalho123@hotmail.com>
2022-02-02 00:26:24 -0300

    Add cors and dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
+import "dotenv";
 import express from "express";
 import { routes as routesAdmin } from "./routes/admin.routes";
 import { routes as routesUser } from "./routes/user.routes";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/admin", routesAdmin);
 app.use(routesUser);
